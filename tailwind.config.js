c20321c69cf7c34e02152dfa9ab0c40817c67916
Nikoloz Parkosadze <nikaparkosadze4@gmail.com>
2024-09-09 17:17:30 +0400

    add necessary tailwind utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                dark: '#232A3C',
+                medium: '#293245',
+                contentDarkBG: '#2B2C37',
+                contentLight: '#F4F7FD',
+            },
+            boxShadow: {
+                boardShadow: '0px 4px 6px 0px rgba(54, 78, 126, 0.10)',
+            },
+        },
+    },
+    plugins: [],
+};
