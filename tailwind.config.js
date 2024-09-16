21e6d3b9cc9f3c4fcd2205aa9b7f7166d4a59e9d
Nikoloz Parkosadze <nikaparkosadze4@gmail.com>
2024-09-16 20:08:30 +0400

    add tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    darkMode: 'class',
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: "class",
     theme: {
         extend: {
             colors: {
-                dark: '#232A3C',
-                medium: '#293245',
-                contentDarkBG: '#2B2C37',
-                contentLight: '#F4F7FD',
+                dark: "#232A3C",
+                medium: "#293245",
+                contentDarkBG: "#2B2C37",
+                contentLight: "#F4F7FD",
+                "custom-border": "rgba(130, 143, 163, 0.25)",
+                "new-subtask-light": "rgba(99, 95, 199, 0.10)",
+                "focus-border": "#5A5E70",
             },
             boxShadow: {
-                boardShadow: '0px 4px 6px 0px rgba(54, 78, 126, 0.10)',
+                boardShadow: "0px 4px 6px 0px rgba(54, 78, 126, 0.10)",
             },
         },
     },
