82bff549fcb6cfe99df0bf96b1413d1361fd19af
Nikoloz Parkosadze <nikaparkosadze4@gmail.com>
2024-09-16 20:06:11 +0400

    remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Boards from "./components/Boards";
-import ThemeToggle from "./components/ThemeToggle";
 
 const App = () => {
-    const [isCreating, setIsCreating] = useState<boolean>(false);
-
-    // const handleCreateClick = () => {
-    //     setIsCreating(!isCreating);
-    // };
+    const [selectedBoard] = useState<number>(0);
 
     return (
-        <>
-            <Navbar isCreating={isCreating} />
+        <div>
+            <Navbar selectedBoard={selectedBoard} />
             <Boards />
-            <ThemeToggle />
-        </>
+        </div>
     );
 };
 
